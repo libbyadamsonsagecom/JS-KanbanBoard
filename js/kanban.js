3306fe202768fbe8298ab49eb3df1b0dbf719cc1
LibbyAdamson <56257687+LibbyAdamson@users.noreply.github.com>
2020-03-16 14:25:03 +0000

    First Task

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,47 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  var item = document.createElement("div");
+  item.classList.add("item");
+  item.setAttribute("id", "item-"+order);
+  item.setAttribute("draggable", "true");
+
+item.addEventListener("dragstart", (event) => {
+  event.DataTransfer.setData('text', event.target.getAttribute('id'));
+});
+
+item.addEventListener("dragend", (event) => {
+  event.DataTransfer.clearData();
+});
+
+var input = document.createElement('input');
+item.appendChild(input);
+
+var save_btn = document.createElement('button');
+save_btn.innerHTML = 'Save';
+
+save_btn.addEventListener('click', () => {
+  error.innerHTML = "";
+  if (input.value !== "") {
+    order += 1;
+    item.innerHTML = input.value;
+    adding = false;
+  } else {
+    error.innerHTML = message;
+  }
+});
+
+item.appendChild (save_btn);
+return item;
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const id = event.DataTransfer.getData('text');
+    event.target.appendChild(document.getElementById(id));
+  });
+  element.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
 });
